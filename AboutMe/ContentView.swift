f43d9c775dd5521cb74abc3a357b48714ff0d4b8
Emma <emmajcool@outlook.com>
2023-07-13 14:00:28 -0700

    Initial Commit

---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20.0) {Text("Hello, my name is Emma")
+                .font(.title)
+                .background(Rectangle() .foregroundColor(Color(hue: 1.0, saturation: 0.357, brightness: 0.945, opacity: 0.499)))
+            
+            Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+            
+            Button("🐶") {
+                textTitle = "I have a golden retriever named Lily"
+                
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+            }
+            .padding()
+            
+            
+            Button("🏐") {
+                textTitle = "My favorite sport it volleyball"
+                
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+            }
+            .padding()
+            
+            
+            
+            struct ContentView_Previews: PreviewProvider {
+                static var previews: some View {
+                    ContentView()
+                }
+            }
+        }
+    }
+}
